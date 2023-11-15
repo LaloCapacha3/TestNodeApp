eed7ba5714c04ef4804c170ae257626971cf16e2
LaloCapacha3 <donapeluda@hotmail.com>
2023-11-15 07:36:27 -0600

    Primero lo primero

---
+++ app.js
@@ -0,0 +1,7 @@
+const moment = require("moment");
+let i = 5, j = 5;
+console.log("Iniciando...")
+console.log("La suma es "+ (5+10));
+
+let fecha = moment().format("LL");
+console.log("Y la fecha es:", fecha);
